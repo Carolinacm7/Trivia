d934dbe584caa0e8008599250a5a050263ef19fc
Carolinacm7 <61048227+Carolinacm7@users.noreply.github.com>
2022-05-11 14:08:16 -0500

    [add]answers_songs

---
+++ Display/answers_songs.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link href="../style.css" rel="stylesheet" type="text/css" />
+    <title>Respuestas</title>
+    <meta charset="UTF-8">
+
+</head>
+
+<body>
+    <div class="presentacion">
+      <div class="marca">
+        <h2 class="titulo__marca">Respuestas canciones de Disney</h2>
+      </div>
+      <div class="container">
+        <h1 class="titulo">Respuestas Expresiones en Inglés-Comida</h1>
+      <p class="pregunta"> 
+      'Libertad sin vuelta atrás
+¡que mas da!no me importa ya gran tormenta habrá el frío es parte también de mi..."la cancion es de la pelicula de <b>Frozen</b> <br>
+     'Una y otra vez cada ruta que hay sendas que tome me regresarán al lugar aquel dónde anhelo estar y no hay que ir jamás...'la cancion es de la pelicula de <b>Moana</b>.<br>
+    'Fabulosa visión sentimiento divino baja y sube y vuela hacia celestial región...'la cancion es de la pelicula de <b>Aladin</b> <br>
+    
+      </p>
+    
+      <a href="../index.php" id="button" class="boton" >Volver</a>
+    </div>
+      </div>
+</body>
+</html>
