16d2c6441b144ce5de2363f94329305b0480f5ee
Carolinacm7 <61048227+Carolinacm7@users.noreply.github.com>
2022-05-11 14:07:18 -0500

    [add]answers_characters

---
+++ Display/answers_characters.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="../style.css" rel="stylesheet" type="text/css" />
+    <title>Respuestas</title>
+    <meta charset="UTF-8">
+
+</head>
+
+<body>
+    <div class="presentacion">
+        <div class="marca">
+            <h2 class="titulo__marca">Trivia de Disney</h2>
+        </div>
+        <div class="container">
+            <h1 class="titulo">Respuestas Personajes de Disney</h1>
+            <p class="pregunta">
+                "El pasado puede doler pero, tal y como yo lo veo, puedes: o huir de él o aprender"lo dice <b>Rafiki</b>del Rey Leon.<br>
+                "Vaya, vaya, qué hermosas flores tenemos este año. ¡Pero mira! Este está retrasado. Pero apuesto a que cuando florezca, será la más hermosa de todas." lo dice <b>Fa Zhou</b> de Mulan.<br>
+                "Lo unico predecible de la vida es que es impredecible" lo dice <b>Remi</b> de Ratatouille.<br>
+            </p>
+
+            <a href="../index.php" id="button" class="boton">Volver</a>
+        </div>
+    </div>
+</body>
+
+</html>
