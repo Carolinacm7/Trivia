90b1382bb0aec346f5a524b38d58364d009a8cf9
Carolinacm7 <61048227+Carolinacm7@users.noreply.github.com>
2022-05-12 10:37:40 -0500

    [add]answer.php

---
+++ Display/answer.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <meta charset = "UTF-8">
+        <link href="../style.css" rel="stylesheet" type="text/css" />
+        <title>Menu de Categorias</title>
+</head>
+<body>
+  <div class="presentacion">      
+        <div class="container">
+        <h1 class="titulo" id="saludo">Hola!</h1>
+        <h2>Seleccione la categoria</h2> 
+        <h2 class="puntos">Respuestas</h2>
+        <div class="botones">
+          <a class="boton" href="answers_songs.php"> Canciones
+          </a>
+          <a class="boton" href="answers_movies.php"> Peliculas
+          </a>
+          <a class="boton" href="answers_characters.php"> Personajes
+          </a>
+        </div>  
+        </div> 
+    </div>
+        
+<script type= "text/javascript" src="../js/categorias.js"> 
+</script> 
+
+</body>
+</html>
