aa2a36dcff7d2580218eed2ea3da86ec6036a6b5
Carolinacm7 <61048227+Carolinacm7@users.noreply.github.com>
2022-05-12 10:39:25 -0500

    [add]result.php

---
+++ Display/result.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <link href="../style.css" rel="stylesheet" type="text/css" />
+  <title>Menu de Categorias</title>
+</head>
+
+<body>
+  <div class="presentacion">
+      <div class="container">
+        <h1 class="puntajeTitulo" >Tu puntaje es: </h1>
+      <div id="SCORE" class="puntaje"></div>
+      <div class="botones">
+      <a class="botonVolver" href="categories_menu.php"> Volver a Jugar
+      </a>
+      <a class="botonVolver" href="../index.php"> 
+      Nuevo Usuario
+      </a>
+      <a class="botonVolver" href="answer.php"> 
+      Ver Respuestas
+      </a>
+    </div>
+      </div>
+      <img class="imgDecoracion" src="../resources/img/ginius-back.png">
+  </div>
+
+  <script type="text/javascript" src="../Js/result.js">
+  </script>
+
+</body>
+
+</html>
