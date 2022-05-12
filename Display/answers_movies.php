5f464bcbfe41f832bfd01573ba7ad1491b0e634c
Carolinacm7 <61048227+Carolinacm7@users.noreply.github.com>
2022-05-12 10:15:12 -0500

    [add]answers_movies

---
+++ Display/answers_movies.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link href="../style.css" rel="stylesheet" type="text/css" />
+    <title>Respuestas</title>
+    <meta charset="UTF-8">
+
+</head>
+
+<body>
+   
+    <div class="presentacion">
+      <div class="marca">
+        <h2 class="titulo__marca">Trivia de Entretenimiento en Inglés</h2>
+      </div>
+      <div class="container">
+        <h1 class="titulo">Respuestas canciones de Disney</h1>
+      <p class="pregunta">
+
+   "Todos los días, viajaremos a un lugar nuevo, y todas las noches, dormiremos debajo el pez. Nadie nos dirá qué hacer. Solo tú y yo ahí afuera. Libres" la pelicula es <b>Luca</b> <br>
+   "Tu chispa no es tu propósito. La última casilla se llena cuando estás listo para vivir" la pelicula es <b>Soul</b><br>
+   "Hace mucho tiempo, el mundo estaba lleno de maravillas. Fue aventurero. Fue emocionante. Y, sobre todo, había magia."  la pelicula es <b>Unidos (Onward)</b><br>
+      </p>
+      
+      <a href="../index.php" id="button" class="boton" >Volver</a>
+    </div>
+    </div>
+</body>
+</html>
