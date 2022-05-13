3630ccf96a85d21fedf44bcea2c0a0e438cdaf53
Carolinacm7 <61048227+Carolinacm7@users.noreply.github.com>
2022-05-13 17:27:55 -0500

    [add]main.php

---
+++ Display/main.php
@@ -0,0 +1,15 @@
+<div class="presentacion">
+      <div class="marca">
+        <h2 class="titulo__marca">Trivia Disney</h2>
+      </div>
+      <div class="container">
+        <h1 class="titulo">Preguntas Disney</h1>
+      <p class="pregunta">¿Que tanto sabes de Disney ? <br>
+        PONTE A PRUEBA...
+      </p>
+      
+      <input type="text" id="nombre" placeholder="Ingresa tu nombre" class="input" >
+      <a href="display/categories_menu.php" id="button" class="boton" >Continuar</a>
+    </div>
+      </div>
+    <script src="js/home.js"></script>
