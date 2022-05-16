a83a82bec28213c7900ad6ab197cdae132a3b2ad
Carolinacm7 <61048227+Carolinacm7@users.noreply.github.com>
2022-05-16 16:49:18 -0500

    [add]main.php

--- Display/main.php
+++ Display/main.php
@@ -1,15 +1,16 @@
 <div class="presentacion">
-      <div class="marca">
-        <h2 class="titulo__marca">Trivia Disney</h2>
-      </div>
-      <div class="container">
-        <h1 class="titulo">Preguntas Disney</h1>
-      <p class="pregunta">¿Que tanto sabes de Disney ? <br>
-        PONTE A PRUEBA...
-      </p>
-      
-      <input type="text" id="nombre" placeholder="Ingresa tu nombre" class="input" >
-      <a href="display/categories_menu.php" id="button" class="boton" >Continuar</a>
-    </div>
-      </div>
-    <script src="js/home.js"></script>
+<div class="img_genius">
+  <img src="./resources/img/ginius-back.png" alt="genius">
+</div>
+  <div class="container">
+    <h1 class="titulo">¡¡Hola!! Soy el Genio y me gustaria saber que tanto sabes del mundo magico</h1>
+    <img class="logo" src="./resources/img/logo-disney.png" alt="logo-disney">
+    <p class="pregunta">¿Que tanto sabes de Disney ? <br>
+      PONTE A PRUEBA...
+    </p>
+
+    <input type="text" id="nombre" placeholder="Ingresa tu nombre" class="input">
+    <a href="display/categories_menu.php" id="button" class="boton">Continuar</a>
+  </div>
+</div>
+<script src="js/home.js"></script>
