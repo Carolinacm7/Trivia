be34b71857071226e687c4fd6b7383f055e6bebd
Carolinacm7 <61048227+Carolinacm7@users.noreply.github.com>
2022-05-17 14:35:49 -0500

    [test]category_menu

--- Display/categories_menu.php
+++ Display/categories_menu.php
@@ -1,31 +1,34 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-        <meta charset = "UTF-8">
-        <link href="../style.css" rel="stylesheet" type="text/css" />
-        <title>Menu de Categorias</title>
+  <meta charset="UTF-8">
+  <link href="../style.css" rel="stylesheet" type="text/css" />
+  <title>Menu de Categorias</title>
 </head>
+
 <body>
-  <div class="presentacion">      
-    <div class="marca">
-      <h2 class="titulo__marca">Trivia Disney</h2>
+  <div class="presentacion">
+    <div class="img_genius">
+      <img src="../resources/img/ginius-back.png" alt="genius">
     </div>
-        <div class="container">
-        <h1 class="titulo" id="saludo">Bienvenid@</h1>
-        <h2>Selecciona una categoria</h2> 
-        <h2 class="puntos">Por cada respuesta correcta obtendras 5 puntos</h2>
-        <div class="botones">
-          <a class="boton" href="movies.php"> <img src="../resources/img/peliculas.jpg" alt="Peliculas">Peliculas
-          </a>
-          <a class="boton" href="songs.php"><img src="../resources/img/cancion-disney.jpg" alt="Musica-Disney">Canciones
-          </a>
-          <a class="boton" href="characters.php"><img src="../resources/img/Personajes-disney.jpg" alt="personajes">Personajes
-          </a>
-        </div>  
-        </div> 
+    <div class="container">
+      <h1 class="titulo" id="saludo">Bienvenid@ </h1>
+      <h1 class="titulo">Selecciona una categoria</h1>
+      <h2 class="puntos titulo">Por cada respuesta correcta obtendras 5 puntos</h2>
+      <div class="botones">
+        <a class="boton" href="movies.php"> <img class="img-category" src="../resources/img/peliculas.jpg" alt="Peliculas">Peliculas
+        </a>
+        <a class="boton" href="songs.php"><img class="img-category" src="../resources/img/cancion-disney.jpg" alt="Musica-Disney">Canciones
+        </a>
+        <a class="boton" href="characters.php"><img class="img-category" src="../resources/img/Personajes-disney.jpg" alt="personajes">Personajes
+        </a>
+      </div>
     </div>
-        
-<script type= "text/javascript" src="../js/categories.js"> 
-</script> 
+  </div>
+
+  <script type="text/javascript" src="../js/categories.js">
+  </script>
 </body>
+
 </html>
