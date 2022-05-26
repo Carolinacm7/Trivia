a95d8fe214e0e49a7be19509ca52c0ac5f8a7faf
Carolinacm7 <61048227+Carolinacm7@users.noreply.github.com>
2022-05-26 12:40:03 -0500

    [add]img main

--- Display/main.php
+++ Display/main.php
@@ -5,7 +5,8 @@
   <div class="container_home">
     <h1 class="titulo">¡¡Hola!! Soy el Genio y me gustaria saber que tanto sabes del mundo magico</h1>
     <img class="logo" src="./resources/img/logo-disney.png" alt="logo-disney">
-    <p class="pregunta titulo">¿Que tanto sabes de Disney ? <br>
+    <img class="img-phone" src="./resources/img/ginius-back.png" alt="genius">
+    <p class="pregunta ">¿Que tanto sabes de Disney ? <br>
       PONTE A PRUEBA...
     </p>
 
